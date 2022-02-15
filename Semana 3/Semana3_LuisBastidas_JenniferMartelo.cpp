927c648ad280b3365fe5e7c96584cef9664a59f8
lbastidash <98669008+lbastidash@users.noreply.github.com>
2022-02-14 23:55:27 -0500

    Add files via upload

---
+++ Semana 3/Semana3_LuisBastidas_JenniferMartelo.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+double f(double x)
+{
+    double y= exp(pow(x,2));
+    return y;
+}
+double dC(double x)
+{
+    double h=0.01;
+    double df= (f(x+h) - f(x))/h;
+    return df;
+}
+int main()
+{
+    int N=2*20./0.01;
+    double x=-20;
+    
+    std::ofstream *File; // Definimos apuntador
+
+	File = new std::ofstream[2]; // Reservamos memoria
+
+	File[0].open( "MisDatos.txt", std::ofstream::trunc ); // Abrir y dejar listo para escribir
+
+	std::cout << File  << std::endl;
+
+    for(int i=0;i<=N;i++)
+    {
+    File[0] << x << " " << dC(x) << std::endl;
+    x = x +0.01;
+    
+    }
+    File[0].close();
+
+    return 0;
+}
